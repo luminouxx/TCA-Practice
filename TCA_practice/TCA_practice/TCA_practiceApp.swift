955148d3ffd58e1a800284b9c726d6496316fdab
luminoux <yungzin98@naver.com>
2023-03-23 16:06:08 +0900

    :sparkles: 프로젝트 생성

---
+++ TCA_practice/TCA_practice/TCA_practiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCA_practiceApp.swift
+//  TCA_practice
+//
+//  Created by YeongJin Jeong on 2023/03/23.
+//
+
+import SwiftUI
+
+@main
+struct TCA_practiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
